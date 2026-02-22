33ab3cee4eb0eb7b5ef9cb15a91c4f9c8313147f
ks6088ts <ks6088ts@gmail.com>
2026-02-23 06:21:06 +0900

    run list blob on CI

--- src/python/template_github_copilot/settings/azure_blob_storage.py
+++ src/python/template_github_copilot/settings/azure_blob_storage.py
@@ -4,8 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class AzureBlobStorageSettings(BaseSettings):
-    azure_blob_storage_account_url: str = ""
-    azure_blob_storage_container_name: str = ""
+    azure_blob_storage_account_url: str = (
+        "https://stazuredatastore.blob.core.windows.net"
+    )
+    azure_blob_storage_container_name: str = "test"
 
     model_config = SettingsConfigDict(
         env_file=".env",
